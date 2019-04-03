d590fb8b79063d5236cc08525604f82f07401b53
Christopher Braun <mrcrbraun@gmail.com>
2019-04-02 21:04:54 -0500

    Delete click_back.py

--- click_back.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get("https://baseballmonster.com/boxscores.aspx")
-elem1 = driver.find_element_by_name("BACK")
-elem1.click()
-html = driver.page_source
